852c8d8bd5a8123295bee8e7fff72eb71b855df9
Damilare Oyedeji <fuglydami@gmail.com>
2020-12-18 15:49:14 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 import Component from './component'
 
 const App = () => {
-  const [section, setSection] = useState(<Component />)
+  const [isSectionVisible, setIsSectionVisible] = useState(true)
   const [value, setValue] = useState('5 Birthday Today')
   const handleClick = () => {
-    setSection([])
+    setIsSectionVisible(false)
     setValue('0 Birthday Today')
   }
   return (
@@ -14,7 +14,7 @@ const App = () => {
       <main>
         <section className="container">
           <h3>{value}</h3>
-          {section}
+          {isSectionVisible && <Component />}
           <div className="wrapper">
             <button onClick={handleClick}>Clear All</button>
           </div>
